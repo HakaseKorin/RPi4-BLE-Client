277be667a92807c89bb80b2331d509e4bb59a6cb
Arby Gudes <aegudes@gmail.com>
2025-12-02 04:24:44 -0600

    updates fpga_bridge test code

--- fpga_bridge.py
+++ fpga_bridge.py
@@ -1,14 +1,20 @@
 import serial # pip install pyserial
 import time
 
-ser = serial.Serial('/dev/serial0', 115200, timeout=1)
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 time.sleep(2)
 
 while True:
-    data = ser.read(1)
-    if data:
-        print("Received:", data.hex())
-        # after recieving information from the fpga board forward to esp32 for commands
+     if ser.in_waiting:
+        b = ser.read()
+        try:
+            print("Got:", b.decode().strip())
+        except:
+            print("Raw:", b)
+    #data = ser.read()
+    #if data:
+    #    print("Received:", data.hex())
+    #    # after recieving information from the fpga board forward to esp32 for commands
 
 """
 Notes for Usage
